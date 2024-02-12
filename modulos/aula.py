dcb6336ff56a8e157efa9b759a7e4194b1619434
JhoanSB0608 <156533638+JhoanSB0608@users.noreply.github.com.>
2024-02-11 22:21:35 -0500

    refactor: :construction: Se eliminaron y modificaron algunas funciones
    Se elimino la funcion de crear aula, se creo la funcion de vefiricar rutas de entrenamiento, se modifico el menu

--- modulos/aula.py
+++ modulos/aula.py
@@ -24,24 +24,12 @@ def guardar_datos(nombre_archivo, data):
     except Exception as e:
         print(f"Error al guardar los datos en el archivo {nombre_archivo}: {e}")
 
-def crear_aula(lista_aulas):
-    if isinstance(lista_aulas, dict):
-        lista_aulas = [lista_aulas] 
-
-    nombre_aula = input("Ingrese el nombre del aula: ")
-    asignar_ruta = input("Ingrese la ruta de entrenamiento asignada al aula: ").lower()
-
-    aula = {
-        "Nombre": nombre_aula,
-        "Capacidad_Maxima": 33,
-        "Capacidad_Actual": 0,
-        "Ruta_Asignada": asignar_ruta
-    }
-
-    lista_aulas.append(aula)
-    print("Se creó el aula con éxito.")
-    guardar_datos("salasEntreno.json", lista_aulas)
-
+def verificar_ruta_entrenamiento(ruta, lista_aulas):
+    for aula in lista_aulas:
+        ruta_asignada = aula.get('Ruta_Asignada')
+        if ruta_asignada and ruta.lower() == ruta_asignada.lower():
+            return True
+    return False
 
 def listar_aulas(lista_aulas):
     print("Listado de aulas de entrenamiento: ")
@@ -50,48 +38,54 @@ def listar_aulas(lista_aulas):
 
 def asignar_camper_a_ruta(lista_campers, lista_aulas):
     id_camper = int(input("Ingrese el ID del camper: "))
-    asignar_ruta = input("Seleccione la ruta de entrenamiento para el camper: ").lower()
+    asignar_ruta = input("Seleccione la ruta de entrenamiento para el camper: ")
 
-    for camper in lista_campers:
-        if camper.get('estado') == 'aprobado' and camper.get('id') == id_camper:
-            for aula in lista_aulas:
-                if asignar_ruta == aula.get('Ruta_Asignada'):
-                    if aula.get('Capacidad_Actual') < aula.get('Capacidad_Maxima'):
-                        camper['ruta_asignada'] = asignar_ruta
-                        aula['Capacidad_Actual'] += 1
-                        guardar_datos("salasEntreno.json", lista_aulas)
-                        print(f"Camper asignado a la ruta {asignar_ruta}.")
-                        return
-                    else:
-                        print(f"No hay espacios disponibles en la ruta {asignar_ruta}")
-                        return
-    print(f"Ruta de entrenamiento {asignar_ruta} no encontrada o camper no aprobado")
+    if verificar_ruta_entrenamiento(asignar_ruta, lista_aulas):
+        for camper in lista_campers:
+            if camper.get('estado') == 'aprobado' and camper.get('id') == id_camper:
+                for aula in lista_aulas:
+                    if asignar_ruta == aula.get('Ruta_Asignada'):
+                        if aula.get('Capacidad_Actual') < aula.get('Capacidad_Maxima'):
+                            camper['ruta_asignada'] = asignar_ruta
+                            aula['Capacidad_Actual'] += 1
+                            guardar_datos("salasEntreno.json", lista_aulas)
+                            print(f"Camper asignado a la ruta {asignar_ruta}.")
+                            return
+                        else:
+                            print(f"No hay espacios disponibles en la ruta {asignar_ruta}")
+                            return
+        print(f"Camper no aprobado.")
+    else:
+        print(f"Ruta de entrenamiento {asignar_ruta} no encontrada.")
 
 def asignar_trainer_a_ruta(lista_trainers, lista_aulas):
     id_trainer = int(input("Ingrese el ID del trainer para asignarle una ruta de entrenamiento: "))
     asignar_ruta_trainer = input("Ingrese la ruta de entrenamiento que imparte el trainer: ")
 
-    for aula in lista_aulas:
-        if asignar_ruta_trainer == aula.get('Ruta_Asignada'):
-            for trainer in lista_trainers:
-                if not trainer.get('ruta_asignada') and trainer.get('id') == id_trainer:
-                    trainer['ruta_asignada'] = asignar_ruta_trainer
+    if verificar_ruta_entrenamiento(asignar_ruta_trainer, lista_aulas):
+        for aula in lista_aulas:
+            if asignar_ruta_trainer == aula.get('Ruta_Asignada'):
+                for trainer in lista_trainers:
+                    if not trainer.get('ruta_asignada') and trainer.get('id') == id_trainer:
+                        trainer['ruta_asignada'] = asignar_ruta_trainer
 
-                    if 'trainers_asignados' not in aula:
-                        aula['trainers_asignados'] = []
+                        if 'trainers_asignados' not in aula:
+                            aula['trainers_asignados'] = []
 
-                    aula['trainers_asignados'].append({
-                        "Nombre": trainer['Nombre'],
-                        "Apellidos": trainer['Apellidos']
-                    })
+                        aula['trainers_asignados'].append({
+                            "Nombre": trainer['Nombre'],
+                            "Apellidos": trainer['Apellidos']
+                        })
 
-                    guardar_datos("salasEntreno.json", lista_aulas)
-                    print(f"Trainer ha sido asignado a la ruta {asignar_ruta_trainer}.")
-                    return
-                else:
-                    print("El trainer ya está asignado a una ruta.")
-                    return
-    print(f"No existe la ruta {asignar_ruta_trainer}.")
+                        guardar_datos("salasEntreno.json", lista_aulas)
+                        print(f"Trainer ha sido asignado a la ruta {asignar_ruta_trainer}.")
+                        return
+                    else:
+                        print("El trainer ya está asignado a una ruta.")
+                        return
+        print(f"ID de trainer no encontrado.")
+    else:
+        print(f"No existe la ruta {asignar_ruta_trainer}.")
 
 def menu_aula():
     lista_aulas = cargar_datos("salasEntreno.json")
@@ -100,23 +94,20 @@ def menu_aula():
 
     while True:
         print("\n--- Menú ---")
-        print("1. Crear aula")
-        print("2. Listar aulas")
-        print("3. Asignar camper a ruta")
-        print("4. Asignar trainer a ruta")
-        print("5. Salir")
+        print("1. Listar aulas")
+        print("2. Asignar camper a ruta")
+        print("3. Asignar trainer a ruta")
+        print("4. Salir")
 
         opcion = input("Seleccione una opción: ")
 
         if opcion == "1":
-            crear_aula(lista_aulas)
-        elif opcion == "2":
             listar_aulas(lista_aulas)
-        elif opcion == "3":
+        elif opcion == "2":
             asignar_camper_a_ruta(lista_campers, lista_aulas)
-        elif opcion == "4":
+        elif opcion == "3":
             asignar_trainer_a_ruta(lista_trainers, lista_aulas)
-        elif opcion == "5":
+        elif opcion == "4":
             print("¡Hasta luego!")
             break
         else:
