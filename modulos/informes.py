fe0506a61979dc98cc949501e602bf03d5fbba1c
JhoanSB0608 <156533638+JhoanSB0608@users.noreply.github.com>
2024-02-13 21:32:39 -0500

    refactor: :construction: Se agrego una nueva funcion
    Se agrego la funcion de aprobaciones por modulo

--- modulos/informes.py
+++ modulos/informes.py
@@ -1,35 +1,5 @@
 import json
 
-def menu_informes():
-    print(""" 
-  __  __ _____ _   _ _   _       ____  _____       ___ _   _ _____ ___  ____  __  __ _____ ____  
- |  \/  | ____| \ | | | | |     |  _ \| ____|     |_ _| \ | |  ___/ _ \|  _ \|  \/  | ____/ ___| 
- | |\/| |  _| |  \| | | | |     | | | |  _|        | ||  \| | |_ | | | | |_) | |\/| |  _| \___ \ 
- | |  | | |___| |\  | |_| |     | |_| | |___       | || |\  |  _|| |_| |  _ <| |  | | |___ ___) |
- |_|  |_|_____|_| \_|\___/      |____/|_____|     |___|_| \_|_|   \___/|_| \_\_|  |_|_____|____/ 
-                                                                                                 
- """)
-    print("1. Campers inscritos")
-    print("2. Campers que aprobaron el examen inicial")
-    print("3. Entrenadores trabajando")
-    print("4. Campers de bajo rendimiento")
-    print("5. Camper y entrenador en ruta entrenamiento")
-    print("6. Salir")
-    opcion = input("Seleccione una opción: ")
-
-    if opcion == "1":
-        listar_campers_inscritos()
-    elif opcion == "2":
-        listar_campers_aprobados_inicial()
-    elif opcion == "3":
-        listar_entrenadores_trabajando()
-    elif opcion == "4":
-        listar_campers_bajo_rendimiento()
-    elif opcion == "5":
-        listar_camper_y_entrenador_en_ruta()
-    elif opcion == "6":
-        print("¡Hasta luego!")
-
 def listar_campers_inscritos():
     for camper in campers_list:
         if camper["Estado"] == "Inscrito":
@@ -65,10 +35,67 @@ def listar_camper_y_entrenador_en_ruta():
                 for camper in asignacion['Campers']:
                     print(f"    - {camper}")
 
+def listar_aprobaciones_por_modulo():
+    for ruta in rutas_entrenamiento_list:
+        print(f"\nRuta de Entrenamiento: {ruta['Nombre']}")
+        for asignacion in ruta.get("Asignaciones", []):
+            entrenador = asignacion.get("Entrenador", "")
+            print(f"\nEntrenador: {entrenador}")
+            for modulo, campers in asignacion.get("Campers_por_modulo", {}).items():
+                aprobados = 0
+                perdidos = 0
+                for camper in campers:
+                    prueba_camper = next((p for p in pruebas_list if p["Nombre"] == camper), None)
+                    if prueba_camper:
+                        promedio = (prueba_camper["Nota_Teorica"] + prueba_camper["Nota_Practica"]) / 2
+                        if promedio >= 60:
+                            aprobados += 1
+                        else:
+                            perdidos += 1
+                print(f"\nMódulo: {modulo}")
+                print(f"Cantidad de Campers Aprobados: {aprobados}")
+                print(f"Cantidad de Campers Perdidos: {perdidos}")
+
+def menu_informes():
+    print(""" 
+  __  __ _____ _   _ _   _       ____  _____       ___ _   _ _____ ___  ____  __  __ _____ ____  
+ |  \/  | ____| \ | | | | |     |  _ \| ____|     |_ _| \ | |  ___/ _ \|  _ \|  \/  | ____/ ___| 
+ | |\/| |  _| |  \| | | | |     | | | |  _|        | ||  \| | |_ | | | | |_) | |\/| |  _| \___ \ 
+ | |  | | |___| |\  | |_| |     | |_| | |___       | || |\  |  _|| |_| |  _ <| |  | | |___ ___) |
+ |_|  |_|_____|_| \_|\___/      |____/|_____|     |___|_| \_|_|   \___/|_| \_\_|  |_|_____|____/ 
+                                                                                                 
+ """)
+    print("1. Campers inscritos")
+    print("2. Campers que aprobaron el examen inicial")
+    print("3. Entrenadores trabajando")
+    print("4. Campers de bajo rendimiento")
+    print("5. Camper y entrenador en ruta entrenamiento")
+    print("6. Aprobaciones por Módulo")
+    print("7. Salir")
+    opcion = input("Seleccione una opción: ")
+
+    if opcion == "1":
+        listar_campers_inscritos()
+    elif opcion == "2":
+        listar_campers_aprobados_inicial()
+    elif opcion == "3":
+        listar_entrenadores_trabajando()
+    elif opcion == "4":
+        listar_campers_bajo_rendimiento()
+    elif opcion == "5":
+        listar_camper_y_entrenador_en_ruta()
+    elif opcion == "6":
+        listar_aprobaciones_por_modulo()
+    elif opcion == "7":
+        print("¡Hasta luego!")
+
 def cargar_datos_desde_json(nombre_archivo):
     try:
         with open(nombre_archivo, 'r') as file:
             data = json.load(file)
+            if nombre_archivo == "data/rutas_entrenamiento.json":
+                for ruta in data:
+                    ruta["Asignaciones"] = {}  
         return data
     except json.JSONDecodeError as e:
         print(f"Error al cargar datos desde JSON: {e}")
